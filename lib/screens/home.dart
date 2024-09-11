9b50bac67acb42f735df0f288f51ca7ec87c7e76
christopheprigent2 <christopheprigent2@gmail.com>
2024-09-11 18:20:36 +0000

    flutter upgrade

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import '../main.dart';
 
 class FirstScreen extends StatelessWidget {
   const FirstScreen({super.key});
--- lib/screens/scores.dart
+++ lib/screens/scores.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../main.dart';
 
 class Scores extends StatelessWidget {
   const Scores({super.key});
