bb57e5c828872667703156c05371091581c15817
christopheprigent2 <christopheprigent2@gmail.com>
2024-09-15 08:39:36 +0000

    auto fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:myapp/screens/scores.dart';
-import 'package:myapp/screens/home.dart';
 
 void main() {
   testWidgets('Score page has a back button', (WidgetTester tester) async {
